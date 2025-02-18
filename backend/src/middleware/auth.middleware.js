69c1adf17527e5ed95637c6856a150c6798d42d6
Avik Dangar <123013809+MyselfAvik@users.noreply.github.com>
2025-02-19 01:44:50 +0530

    some changes

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -30,8 +30,8 @@ export const protectRoute=async(req,res,next)=>{
         }
 
 
-        req.user=user
-        next()
+        req.user=user;
+        next();
     } catch (error) {
         console.log("error in protectRoute middleware",error.message)
         return res.status(500).json({message:"Internal Server Error"})
