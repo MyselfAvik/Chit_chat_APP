b7d260a71d1f2c7efa0539dc5c7ab3c9e6373235
Avik Dangar <123013809+MyselfAvik@users.noreply.github.com>
2025-02-19 02:55:26 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,17 +23,19 @@ app.use(cors({
     credentials:true,
 }   
 )) 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    });
-  }
+
 
   
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 server.listen(PORT,()=>{
     console.log("running",process.env.PORT);
     connectDB()
